193d283f29d057d54510ebfd3f53e23f854c309d
Akhila-github-dev <akhilahere3@gmail.com>
2026-03-15 19:12:37 +0530

    spring security2

---
+++ java/com/springSecurity/controller/AcconttController.java
@@ -0,0 +1,15 @@
+package com.springSecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AcconttController {
+
+	
+	@GetMapping(value ={"/" ,"/account"})
+	public String get() {
+		return "hello how are you";
+	}
+}
